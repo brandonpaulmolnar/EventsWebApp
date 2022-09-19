8fd6856dbe93206e657654f870afa704d5ce81b4
Brandon Molnar <brandonpaulmolnar@gmail.com>
2022-09-18 18:19:14 -0700

    Add project files.

---
+++ EventsWebApp/Data/Migrations/20220917212151_initialsetup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventsWebApp.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Event",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Event", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Event");
+        }
+    }
+}
---
+++ EventsWebApp/Models/Event.cs
@@ -0,0 +1,17 @@
+﻿namespace EventsWebApp.Models
+{
+    public class Event
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+
+        public Event()
+        {
+
+        }
+
+    }
+}
